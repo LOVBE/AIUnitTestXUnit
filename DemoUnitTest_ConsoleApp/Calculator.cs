771562035af1dbc5708f733cc4c8e801b841e2e4
LOVBE <tranludangtrung@gmail.com>
2025-10-08 16:41:49 +0700

    first push

---
+++ DemoUnitTest_ConsoleApp/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoUnitTest_ConsoleApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException("Cannot divide by zero.");
+            return a / b;
+        }
+
+        public int Modulo(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException("Cannot modulo by zero.");
+            return a % b;
+        }
+    }
+}
---
+++ UnitTest/UnitTest_Generated.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Xunit;
+
+namespace DemoUnitTest_ConsoleApp.Tests
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator = new Calculator();
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-5, 5, 0)]
+        [InlineData(int.MaxValue, 0, int.MaxValue)]
+        public void Add_ReturnsCorrectSum(int a, int b, int expected)
+        {
+            var result = _calculator.Add(a, b);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(5, 3, 2)]
+        [InlineData(-10, -5, -5)]
+        [InlineData(int.MinValue, 1, int.MinValue + 1)]
+        public void Subtract_ReturnsCorrectDifference(int a, int b, int expected)
+        {
+            var result = _calculator.Subtract(a, b);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(4, 5, 20)]
+        [InlineData(-3, 6, -18)]
+        [InlineData(int.MaxValue / 2, 2, int.MaxValue)]
+        public void Multiply_ReturnsCorrectProduct(int a, int b, int expected)
+        {
+            var result = _calculator.Multiply(a, b);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(10, 2, 5)]
+        [InlineData(-9, -3, 3)]
+        [InlineData(int.MaxValue, 1, int.MaxValue)]
+        public void Divide_ReturnsCorrectQuotient_WhenDivisorNonZero(int a, int b, int expected)
+        {
+            var result = _calculator.Divide(a, b);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Divide_ThrowsDivideByZeroException_WhenDivisorIsZero()
+        {
+            Assert.Throws<DivideByZeroException>(() => _calculator.Divide(10, 0));
+        }
+
+        [Theory]
+        [InlineData(10, 3, 1)]
+        [InlineData(-7, -4, -3)]
+        [InlineData(int.MaxValue, 1, 0)]
+        public void Modulo_ReturnsCorrectRemainder_WhenDivisorNonZero(int a, int b, int expected)
+        {
+            var result = _calculator.Modulo(a, b);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Modulo_ThrowsDivideByZeroException_WhenDivisorIsZero()
+        {
+            Assert.Throws<DivideByZeroException>(() => _calculator.Modulo(10, 0));
+        }
+    }
+}
